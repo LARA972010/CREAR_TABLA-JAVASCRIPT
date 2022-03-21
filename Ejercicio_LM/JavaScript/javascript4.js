1422cfc42a69f2563b1b6e13cd1a710d9105ed98
LARA <92118085+LARA972010@users.noreply.github.com>
2022-03-21 09:10:30 +0100

    Add files via upload

---
+++ Ejercicio_LM/JavaScript/javascript4.js
@@ -0,0 +1,33 @@
+
+var amigos=[{nombre:"Pepe", apellido:"Pérez"},
+{nombre:"Elena", apellido:"Nito"},
+{nombre:"Dolores", apellido:"Fuertes"}];
+
+function crearTablaAmigos(){
+    var obj_tabla = document.createElement("table");
+    obj_tabla.className="tabla";
+    for (i=0; i<amigos.length; i++){
+        var obj_fila = document.createElement("tr");
+        var obj_celda_nombre=document.createElement("td");
+        var obj_celda_apellido=document.createElement("td");
+        obj_celda_nombre.innerHTML="<strong>"+amigos[i].nombre+"</strong>";
+        obj_celda_apellido.innerHTML="<strong>"+amigos[i].apellido+"</strong>";
+        obj_fila.appendChild(obj_celda_nombre);
+        obj_fila.appendChild(obj_celda_apellido);
+        obj_tabla.appendChild(obj_fila);
+
+
+    }
+    document.getElementById("contenedor_amigos").appendChild(obj_tabla);
+
+    
+
+
+
+
+}
+
+
+
+
+
